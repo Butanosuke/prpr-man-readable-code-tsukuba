290161c24e51fd1ed683a645cdc13aa2466ad0a9
prpr-man <oharada@live.jp>
2015-07-04 11:30:27 +0900

    spec3: 課題3の実装およびrecipe-data.txtを追加

--- recipe.cpp
+++ recipe.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
+
+string getRecipeName(string recipe_file_path){
+  ifstream recipe_file(recipe_file_path);
+  if(recipe_file.fail()){
+    cout << "レシピファイルが見つかりません" << endl;
+    exit(EXIT_FAILURE);
+  }
+  
+  string recipe_name;
+  recipe_file >> recipe_name;
+
+  return recipe_name;
+}
 
 int main()
 {
-  cout << "オムライス" << endl;
+  string recipe_name = getRecipeName("data/recipe-data.txt");
 
-  return 0;
+  cout << recipe_name << endl;
+  
+  exit(EXIT_SUCCESS);
 }
